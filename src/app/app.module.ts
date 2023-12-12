886a7f67cf0c4c7fa5ac5d3a1582a4924b20ac75
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-12-12 16:09:13 -0500

    ROUTER MODULE
    ADD router module y creando los enlaces class app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes:Routes = [
+  {path:'', redirectTo:'/inicio', pathMatch:'full'},
+  {path:'inicio', component:InicioComponent},
+  {path:'clientes', component: ClientesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    ClientesComponent
+    ClientesComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes)
     
   ],
   providers: [
