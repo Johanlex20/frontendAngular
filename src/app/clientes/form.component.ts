610a64a310ae85f544d25cbc5414b00455d73bc1
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-12-13 14:11:10 -0500

    Installa Sweet Alert 2
    Agregando el mensaje en la clase form component

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -22,7 +23,10 @@ export class FormComponent implements OnInit{
       //console.log("click");
       //console.log(this.cliente);
       this.clienteService.create(this.cliente).subscribe(
-        response => this.router.navigate(['/clientes'])
+        cliente => {
+          this.router.navigate(['/clientes'])
+          Swal.fire('Nuevo Cliente',`Cliente ${ cliente.nombre } creado con éxito!`, 'success')
+        }
       )
   }
 
