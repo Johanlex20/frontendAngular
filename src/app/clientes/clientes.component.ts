561c4e30fd7fc5c32323526aab0ba9063c528769
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-12-13 15:09:19 -0500

    Creando el Metodo delete
    En la  clase serivies y despues en cliente component directamente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { map } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -18,6 +19,45 @@ ngOnInit(){
   this.clienteService.getAll().subscribe(
     clientes=> this.clientes = clientes
   )
+}
+
+delete(cliente: Cliente) :void {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger"
+    },
+    buttonsStyling: false
+  });
+  swalWithBootstrapButtons.fire({
+    title: "Esta seguro?",
+    text: `Seguro que desea eliminar al cliente ${(cliente.nombre)} ${(cliente.apellido)}?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Si, Eliminar!",
+    cancelButtonText: "No, cancelar!",
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+      this.clienteService.delete(cliente.id).subscribe(
+        response => {
+
+          this.clientes = this.clientes.filter(cli => cli !== cliente) 
+
+          swalWithBootstrapButtons.fire({
+            title: "Cliente Eliminado",
+            text: `Cliente ${(cliente.nombre)} Eliminado con Éxito.`,
+            icon: "success"
+          });
+        }
+      )
+
+
+
+    } 
+  });
+  
 }
 
 }
