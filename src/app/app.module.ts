71ca3ace197558174c5bb975c1f37964a07a8b20
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-01-05 10:43:46 -0500

    cread PaginatorComponet, add link en tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
+import { PaginatorComponent } from './paginator/paginator.component';
 
 
 const routes:Routes = [
@@ -32,7 +33,8 @@ const routes:Routes = [
     HeaderComponent,
     ClientesComponent,
     InicioComponent,
-    FormComponent
+    FormComponent,
+    PaginatorComponent
   ],
   imports: [
     BrowserModule,
